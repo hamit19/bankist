4c50e686a8c1e51a6b4fc40e83e48d7acf9af834
Hamid Hassani <hamidhassani119@gmail.com>
2024-04-07 11:58:03 +0330

    The request loan function has been implemented now the user can request laon if the amount of the loan is greater than 10 percent of any deposite

--- script.js
+++ script.js
@@ -199,6 +199,23 @@ btnClose.addEventListener("click", function (e) {
   inputCloseUsername.value = inputClosePin.value = "";
 });
 
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => amount >= mov * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+
+    inputLoanAmount.value = "";
+  }
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
